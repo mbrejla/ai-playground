dfef5ff8f222de7aab9c305d586f10e87235edf6
Markus Brejla <m.brejla@gfx3d.net>
2025-06-15 23:22:36 +0200

    basic ai interface

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,12 +8,28 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 model = "gemini-2.0-flash-001"
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+verbose = False
 
-answer_object = client.models.generate_content(model=model, contents=prompt)
+if len(sys.argv) <= 1:
+    print('Error: No prompt given.\nUsage: python3 main.py "Your prompt here."')
+    sys.exit(1)
+if not isinstance(sys.argv[1], str) or sys.argv[1].startswith("--"):
+    print("Error: Prompt must be the first parameter")
+    sys.exit(1)
+
+if "--verbose" in sys.argv:
+    verbose = True
+
+prompt = sys.argv[1]
+messages = [
+    genai.types.Content(role="user", parts=[genai.types.Part(text=prompt)]),
+]
+answer_object = client.models.generate_content(model=model, contents=messages)
 meta = answer_object.usage_metadata
 
 
 print(answer_object.text)
-print(f"Prompt tokens: {meta.prompt_token_count}")
-print(f"Response tokens: {meta.candidates_token_count}")
+if verbose:
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {meta.prompt_token_count}")
+    print(f"Response tokens: {meta.candidates_token_count}")
