4972366525103301d2f877af93f639cad828ecb3
Markus Brejla <m.brejla@gfx3d.net>
2025-06-15 23:43:58 +0200

    cleanup

--- main.py
+++ main.py
@@ -3,33 +3,37 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-model = "gemini-2.0-flash-001"
 
-verbose = False
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    model = "gemini-2.0-flash-001"
 
-if len(sys.argv) <= 1:
-    print('Error: No prompt given.\nUsage: python3 main.py "Your prompt here."')
-    sys.exit(1)
-if not isinstance(sys.argv[1], str) or sys.argv[1].startswith("--"):
-    print("Error: Prompt must be the first parameter")
-    sys.exit(1)
+    verbose = False
+    args = sys.argv[1:]
 
-if "--verbose" in sys.argv:
-    verbose = True
+    if not args or args[0] == "" or args[0].startswith("--"):
+        print(
+            'Error: No correct prompt given.\nUsage: python3 main.py "Your prompt here." [--verbose]'
+        )
+        sys.exit(1)
+    if "--verbose" in sys.argv:
+        verbose = True
 
-prompt = sys.argv[1]
-messages = [
-    genai.types.Content(role="user", parts=[genai.types.Part(text=prompt)]),
-]
-answer_object = client.models.generate_content(model=model, contents=messages)
-meta = answer_object.usage_metadata
+    prompt = args[0]
+    messages = [
+        genai.types.Content(role="user", parts=[genai.types.Part(text=prompt)]),
+    ]
+    answer_object = client.models.generate_content(model=model, contents=messages)
+    meta = answer_object.usage_metadata
+
+    print(answer_object.text)
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {meta.prompt_token_count}")
+        print(f"Response tokens: {meta.candidates_token_count}")
 
 
-print(answer_object.text)
-if verbose:
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {meta.prompt_token_count}")
-    print(f"Response tokens: {meta.candidates_token_count}")
+if __name__ == "__main__":
+    main()
